64a080d538f99174c2c5d2c853bd20ca1e495068
Luis Fernando Maschietto Junior <maschietto.jr@gmail.com>
2023-03-06 02:00:38 +0000

    Working here

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const userController = require('./controllers/User');
-require('dotenv').config();
-console.log(process.env.MONGO_URI)
-mongoose.connect(process.env.MONGO_URI).then(result => {
-  console.log('result: ', result)
-}).catch(error=>console.log('Error connecting to MongoDB: ', error));
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const userController = require("./controllers/User");
+require("dotenv").config();
+console.log(process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((result) => {
+    console.log("MongoDB connected!");
+  })
+  .catch((error) => console.log("Error connecting to MongoDB: ", error));
 
 app.use(cors());
 app.use(bodyParser());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.post('/api/users', async (req, res) => {
+app.post("/api/users", async (req, res) => {
   const newUser = await userController.add(req.body.username);
   res.json(newUser);
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  const newUser = await userController.add(req.body.username);
+  res.json(newUser);
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port);
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
